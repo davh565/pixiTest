66106ee3e482712a3312ac26f2a21de21b18fd45
David Humphries <d.a.humphries@gmail.com>
2020-04-16 20:03:48 +1200

    refactoring

--- _.js
+++ _.js
@@ -1,17 +1,17 @@
 register = {
-    tag: {},
-    path: {},
+    tags: {},
+    paths: {},
     add: function(thing){
-        for(let location in thing.locations){
-            let path = thing.locations[location]
-            if(this.path[path] == undefined){
+        for(let index in thing.locations){
+            let path = thing.locations[index]
+            if(this.paths[path] == undefined){
 
-                this.path[path] = {}
-                this.path[path].occupants = {}
+                this.paths[path] = {}
+                this.paths[path].occupants = {}
             }
-            this.path[path].occupants[thing.id] = thing
+            this.paths[path].occupants[thing.id] = thing
         }
-        this.tag[thing.id] = thing
+        this.tags[thing.id] = thing
         return this
     },
     move: function (tag,address) {
@@ -30,31 +30,25 @@ register = {
         for(let index in thing.locations){
             let location = register.findLocation(thing.locations[index])
             delete location.occupants[tag]
-            delete register.tag[tag]
+            delete register.tags[tag]
         }
     },
-    scale: function(tag){
-        let thing = register.findTag(tag) 
-
-    },
     findLocation: function(location){
-        return this.path[location]
+        return this.paths[location]
     },
     findTag: function(tag){
-        return this.tag[tag]
-    }
-        ,
-    
+        return this.tags[tag]
+    },
+    validate: function(){},
 }
 thingProto = {
     generateId: function(name){
         let index = 0
-        while(register.tag[name+toHexString(index)] != undefined){
+        while(register.tags[name+toHexString(index)] != undefined){
             index++
         }
         return name+toHexString(index)
     },
-    //reserved middleware functions
     addThing: function(){},
     moveThing: function(){},
     scaleThing: function(){},
@@ -72,14 +66,15 @@ function toHexString (index){
     return index.toString(16).padStart(2,0)
 }
 
-let register = r
+
 createThing("bobby","grid01_tile0212")
 createThing("bobby","grid01_tile0212")
 createThing("billy","grid01_tile0212")
 createThing("kim","grid01_tile0000")
 let kim = register.findTag("kim00")
-r.move("kim00","grid01_tile0212")
-r.delete("bobby00")
-console.log(r)
-// console.log(register.findLocation("grid01_tile0212").occupants)
-// console.log(register.findTag("bobby01"))
+register.move("kim00","grid01_tile0212")
+register.delete("bobby00")
+console.log(register)
+console.log(register.findLocation("grid01_tile0212").occupants)
+console.log(register.findTag("bobby01"))
+console.log(register.findTag("bobby00"))
